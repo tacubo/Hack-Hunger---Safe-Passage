2fa30b842444657834b54f72fac1ff186d6b43dc
Alberto Lobato <Alberto.Lobato@riseinteractive.com>
2016-09-24 15:34:06 -0500

    blah

--- index.php
+++ index.php
@@ -1,91 +1,91 @@
         <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml">
         <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Safe Passage</title>
-        <script src="//code.jquery.com/jquery-latest.min.js" type="application/javascript"></script>
-        <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyCFZyhCp8lqfIeognHqe-iauOZLEhhzYjY&sensor=false"></script>
-        <script>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <title>Safe Passage</title>
+          <script src="//code.jquery.com/jquery-latest.min.js" type="application/javascript"></script>
+          <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyCFZyhCp8lqfIeognHqe-iauOZLEhhzYjY&sensor=false"></script>
+          <script>
             $(document).ready(function(){
                 var markers = []; // define global array in script tag so you can use it in whole page    
                 var infowindow = new google.maps.InfoWindow();
                 var bounds = new google.maps.LatLngBounds();
                 var myCenter=new google.maps.LatLng(41.881832,-87.623177);
                 var mapProp = {
-                        center:myCenter, 
-                        zoom:11,
-                        minZoom:6,          
-                        mapTypeId : google.maps.MapTypeId.ROADMAP,
-                        mapTypeControl: true           
-                    };
-                 function infoContent(info){
+                  center:myCenter, 
+                  zoom:11,
+                  minZoom:6,          
+                  mapTypeId : google.maps.MapTypeId.ROADMAP,
+                  mapTypeControl: true           
+                };
+                function infoContent(info){
                   var infoString = '<h1>' + info.name + '</h1><br><b>' + 'Address:</b> ' + info.address + '<br><b>School:</b> ' + isTrue(info.school) + '<br><b>Breakfast:</b> ' + isAvailable(info.breakfast) + '<br><b>Lunch:</b> ' + isAvailable(info.lunch) + '<br><b>Supper:</b> ' + isAvailable(info.supper) + '<br><b>PM Snack:</b> ' + isAvailable(info.snack) + '<br><b>Distance from Safe Passage: </b> ' + info.distance;
                   return infoString;
-                 }
+                }
 
-                 function isTrue(data){
-                    if (data == 0){
-                      return 'False'
-                    }else{
-                      return 'True'
-                    }
-                 }
+                function isTrue(data){
+                  if (data == 0){
+                    return 'False'
+                  }else{
+                    return 'True'
+                  }
+                }
 
-                 function isAvailable(data){
-                    if (data == 0){
-                      return 'Not Available'
-                    }else{
-                      return 'Available'
-                    }
-                 }
+                function isAvailable(data){
+                  if (data == 0){
+                    return 'Not Available'
+                  }else{
+                    return 'Available'
+                  }
+                }
                 //google map object       
-                 var map = new google.maps.Map(document.getElementById("gMap"),mapProp);
-                 map.data.loadGeoJson('CPS_Safe_Passage_Routes_SY1516.geojson'); 
-                 var markercsv = "DistroSitesV6.csv";
+                var map = new google.maps.Map(document.getElementById("gMap"),mapProp);
+                map.data.loadGeoJson('CPS_Safe_Passage_Routes_SY1516.geojson'); 
+                var markercsv = "DistroSitesV6.csv";
 
-                            var reader = new FileReader();
-                            reader.onload = function(markercsv) {
 
-                                      var csvval=e.target.result.split("\n");
-                                      var csvvalue;                                          
-
-                                      for(var i = 0;i < csvval.length;i++)
-                                      {
-                                              markers[i] = []; 
-                                              csvvalue = csvval[i].split(",");
-                                              markers[i][0] = csvvalue[0]; //id
+                (function() {
+                  var depositoriesApi = "http://10.1.106.135:8080/depositories";
+                  $.getJSON( depositoriesApi, {
+                    format: "json"
+                  })
+                  .done(function( data ) {
+                    $.each( data.items, function( i, item ) {
+                      markers[i] = []; 
+                      item = item[i].split(",");
+                                              markers[i][0] = item.name; //id
                                               info = [];
-                                              info.name = csvvalue[0];
-                                              info.address = csvvalue[1] + ' ' + csvvalue[2] + ', ' + csvvalue[3] + ' ' + csvvalue[4];
-                                              info.school = csvvalue[7];
-                                              info.breakfast = csvvalue[8];
-                                              info.lunch = csvvalue[9];
-                                              info.supper = csvvalue[10];
-                                              info.snack = csvvalue[11];
+                                              info.name = item.name;
+                                              info.address = item.address + ' ' + item.city + ', ' + item.state + ' ' + item.zip;
+                                              info.school = item.isSchool;
+                                              info.breakfast = item.hasBreakfast;
+                                              info.lunch = item.hasLunch;
+                                              info.supper = item.hasSupper;
+                                              info.snack = item.hasPmSnack;
                                               info.distance = "1 miles"
-                                              var name = csvvalue[0];
-                                              var lat = csvvalue[5]; //latitude
-                                              var lng = csvvalue[6]; //longitude
+                                              var name = item.name;
+                                              var lat = item.yCoordinate; //latitude
+                                              var lng = item.xCoordinate; //longitude
                                               markers[i][1] = new google.maps.Marker({
-                                                   position: new google.maps.LatLng(lat,lng),
-                                                   map: map,
-                                                   title: name,
-                                                   info: info,                  
-                                              });
+                                               position: new google.maps.LatLng(lat,lng),
+                                               map: map,
+                                               title: name,
+                                               info: info,                  
+                                             });
                                               google.maps.event.addListener(markers[i][1], 'click', function () {
                                                 infowindow.setContent(infoContent(this.info));
-                                                  infowindow.open(map, this);
+                                                infowindow.open(map, this);
                                               });
-                                       }
+                                            });
+                  });
+                })();
 
-                             };
-                             reader.readAsText(markercsv);
-            });
-        </script>
-        </head>
 
-        <body>
-            <div style="height:700px; width:1500px;" id="gMap">
-            </div>
-        </body>
-        </html>
+                                      </script>
+                                    </head>
+
+                                    <body>
+                                      <div style="height:700px; width:1500px;" id="gMap">
+                                      </div>
+                                    </body>
+                                    </html>
