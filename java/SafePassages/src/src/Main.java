a8d3eab8008e8073e03ea7d9a1e731f5be590b7b
Oliver Gugenheim <lees@ripco.com>
2016-09-24 13:12:47 -0500

    ojplg - print school names

--- java/SafePassages/src/src/Main.java
+++ java/SafePassages/src/src/Main.java
@@ -1,7 +1,10 @@
 
+import query.SafePassages;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 public class Main {
@@ -10,17 +13,9 @@ public class Main {
 
         try {
 
-            Class.forName("org.postgresql.Driver");
-            System.out.println("Found driver");
+            Connection connection = connectToDatabase();
+            printOutSchoolNames(connection);
 
-            String url = "jdbc:postgresql://10.1.106.228/safepassage";
-            Properties props = new Properties();
-
-            props.setProperty("user", "safepassageuser");
-            props.setProperty("password", "safepassage");
-            //props.setProperty("ssl", "true");
-            Connection conn = DriverManager.getConnection(url, props);
-            System.out.println("connected " + conn);
         } catch (SQLException se){
             System.out.println("Error getting connection");
             se.printStackTrace();
@@ -29,4 +24,28 @@ public class Main {
             cnfe.printStackTrace();
         }
     }
+
+    private static Connection connectToDatabase() throws SQLException, ClassNotFoundException {
+        System.out.println("Connecting to DB.");
+        Class.forName("org.postgresql.Driver");
+        System.out.println("Found driver");
+
+        String url = "jdbc:postgresql://10.1.106.228/safepassage";
+        Properties props = new Properties();
+
+        props.setProperty("user", "safepassageuser");
+        props.setProperty("password", "safepassage");
+        Connection conn = DriverManager.getConnection(url, props);
+        System.out.println("Connected. " + conn);
+        return conn;
+    }
+
+    private static void printOutSchoolNames(Connection connection) throws SQLException {
+        SafePassages sf = new SafePassages();
+        List<String> schoolNames = sf.getSchoolNames(connection);
+        for(String name : schoolNames){
+            System.out.println(name);
+        }
+    }
+
 }
